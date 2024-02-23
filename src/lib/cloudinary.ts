5cd0c549512f6fd3e9617f35592d657c29ec344e
Akshat <akshatjangir1710@gmail.com>
2024-02-23 20:03:20 +0530

    update .env file

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,9 +1,8 @@
 import {v2 as cloudinary} from 'cloudinary';
-//connecting cloudinary for storing the images in link form which is provided by cloudinary by storing the images on their cloud          
+          
 cloudinary.config({ 
-  cloud_name: 'CLOUD_NAME', 
-  api_key: 'API_KEY', 
-  api_secret: 'API_SECRET' 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
 });
-
 export default cloudinary;
